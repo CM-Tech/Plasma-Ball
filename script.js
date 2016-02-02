5b90e83e37071948e9c78ca2a0be41f4cdb84df5
modderme123 <modderme123@gmail.com>
2016-02-01 17:47:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -30,53 +30,47 @@ for (var i = 0; i < 30; i++) {
 }
 
 function tick() {
-    var grd = ctx.createRadialGradient(250, 250, 50, 250, 250, 100);
-    grd.addColorStop(0, "purple");
-    grd.addColorStop(1, "rgba(213, 49, 119,0.7)");
-    ctx.strokeStyle = grd;
-
+    var beforeLinesCanvas = document.createElement("canvas");
+    beforeLinesCanvas.width = c.width;
+    beforeLinesCanvas.height = c.height;
+    var beforeLinesCanvasCtx = beforeLinesCanvas.getContext("2d");
+    var afterLinesCanvas = document.createElement("canvas");
+    afterLinesCanvas.width = c.width;
+    afterLinesCanvas.height = c.height;
+    var afterLinesCanvasCtx = afterLinesCanvas.getContext("2d");
     ctx.clearRect(0, 0, 500, 500);
+    
     ctx.beginPath();
+    ctx.strokeStyle = "rgba(213, 49, 119,0.7)";
     ctx.arc(250, 250, 245, 0, 2 * Math.PI);
     ctx.lineWidth = 5;
     ctx.stroke();
-    ctx.beginPath();
-    ctx.lineWidth = 10;
-    var grd = ctx.createRadialGradient(250, 250, 30, 250, 250, 55);
-    grd.addColorStop(0, "rgb(213, 49, 119)");
-    grd.addColorStop(1, "white");
-    ctx.arc(250, 250, 45, 0, 2 * Math.PI);
-    ctx.fillStyle = grd;
-    ctx.fill();
+
+
     ctx.lineCap = "round";
     for (var i = 0; i < lines.length; i++) {
+
         var sinOffset = Math.sin(2 * Math.PI * lineOffsets[i]) / 2;
         var pitchM = Math.sin(2 * Math.PI * linePitches[i]);
-        var grd = ctx.createRadialGradient(250, 250, Math.abs(110 * pitchM), 250, 250, Math.abs(300 * pitchM));
+        var renderOn = ctx;
+        if (linePitches[i] < 0.5) {
+            renderOn = afterLinesCanvasCtx;
+        }
+        renderOn.lineCap = "round";
+        var grd = renderOn.createRadialGradient(250, 250, Math.abs(130 * pitchM), 250, 250, Math.abs(300 * pitchM));
         grd.addColorStop(0, "#02f");
         grd.addColorStop(1, "white");
-        ctx.strokeStyle = grd;
+        renderOn.strokeStyle = grd;
         var end = {
             x: 250 + Math.cos(2 * Math.PI * lines[i]) * 245 * pitchM,
             y: 250 + Math.sin(2 * Math.PI * lines[i]) * 245 * pitchM
         };
-        ctx.beginPath();
-        ctx.lineWidth = 5;
-        ctx.moveTo(250 + Math.cos(2 * Math.PI * lines[i]) * 45 * pitchM, 250 + Math.sin(2 * Math.PI * lines[i]) * 45 * pitchM);
-        ctx.quadraticCurveTo(250 + Math.cos(2 * Math.PI * lines[i] + sinOffset / 2) * 200 * pitchM, 250 + Math.sin(2 * Math.PI * lines[i] + sinOffset / 2) * 200 * pitchM, 250 + Math.cos(2 * Math.PI * lines[i]) * 245 * pitchM, 250 + Math.sin(2 * Math.PI * lines[i]) * 245 * pitchM);
-
-        //ctx.lineTo(250+Math.cos(2 * Math.PI*lines[i])*245,250+Math.sin(2 * Math.PI*lines[i])*245);
-        ctx.stroke();
-        ctx.beginPath();
-        var grd = ctx.createRadialGradient(end.x, end.y, 1, end.x, end.y, 15);
-        grd.addColorStop(0, "#D53177");
-        grd.addColorStop(1, "rgba(0,0,0,0)");
-        ctx.strokeStyle = grd;
-        ctx.fillStyle = grd;
-        ctx.lineWidth = 10;
-        ctx.arc(end.x, end.y, 5, 0, 2 * Math.PI);
-        ctx.fill();
-        //ctx.stroke();
+        renderOn.beginPath();
+        renderOn.lineWidth = 5;
+        renderOn.moveTo(250 + Math.cos(2 * Math.PI * lines[i]) * 45 * pitchM, 250 + Math.sin(2 * Math.PI * lines[i]) * 45 * pitchM);
+        renderOn.quadraticCurveTo(250 + Math.cos(2 * Math.PI * lines[i] + sinOffset / 2) * 200 * pitchM, 250 + Math.sin(2 * Math.PI * lines[i] + sinOffset / 2) * 200 * pitchM, 250 + Math.cos(2 * Math.PI * lines[i]) * 245 * pitchM, 250 + Math.sin(2 * Math.PI * lines[i]) * 245 * pitchM);
+        renderOn.stroke();
+        fcirc(renderOn, end.x, end.y, 5, 1, 15, "rgba(0,0,0,0)");
         lineDirections[i].x = Math.min(Math.max(lineDirections[i].x, 0), 1);
         lineDirections[i].y = Math.min(Math.max(lineDirections[i].y, 0), 1);
         lines[i] += (lineDirections[i].x - 0.5) * 0.003;
@@ -97,8 +91,20 @@ function tick() {
         lineDirections[i].x += Math.random() / 10 - 0.05;
         lineDirections[i].y += Math.random() / 10 - 0.05;
     }
+    fcirc(ctx,250,250,45,30,55,"white");
+    ctx.drawImage(afterLinesCanvas, 0, 0);
 }
 (function animloop() {
     requestAnimFrame(animloop);
     tick();
 })();
+function fcirc(canva, x, y, size, gradient1, gradient2, ecolor) {
+    canva.beginPath();
+    canva.lineWidth = 10;
+    var grd = ctx.createRadialGradient(x, y, gradient1, x, y, gradient2);
+    grd.addColorStop(0, "rgb(213, 49, 119)");
+    grd.addColorStop(1, ecolor);
+    canva.arc(x, y, size, 0, 2 * Math.PI);
+    canva.fillStyle = grd;
+    canva.fill();
+}
